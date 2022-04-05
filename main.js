d4a8a5680cda8bcf952fabe2ad832991979eb440
gusBrasil <gustavomedbra@gmail.com>
2022-04-04 23:05:27 -0300

    update

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom() {
+document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+function tocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
