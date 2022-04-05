741c4b502677a6b1ff8aac4252ded5ce0de1eca4
gusBrasil <gustavomedbra@gmail.com>
2022-04-05 15:18:18 -0300

    update

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 function tocaSom(seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
-    if (elemento === null) {
-        alert('Elemento não encontrado');
-    }
-
     if (elemento != null && elemento.localName === 'audio') {
         elemento.play();
+    }
+    else {
+        alert('Elemento não encontrado');
     }
 }
 
